e2db547dd41594fd5cb046b9bdceaea44cbb8d16
Sakaguchi123 <chiyumi_sakaguchi@mail.toyota.co.jp>
2024-06-26 22:53:03 +0900

    fix: planエンドポイント変更

--- router/plans.ts
+++ router/plans.ts
@@ -7,4 +7,23 @@ router.get("/", async (req: express.Request, res: express.Response) => {
   res.send(data);
 });
 
+router.post("/", async (req: express.Request, res: express.Response) => {
+  const userId = req.body.user_id;
+  const planId = await knex("plans").insert(
+    {
+      date: req.body.date,
+      state: "募集中",
+      users_count: 1,
+    },
+    "id"
+  );
+
+  await knex("plans_users").insert({
+    plan_id: planId[0].id,
+    user_id: userId,
+  });
+
+  res.status(200).json({ message: "ok" });
+});
+
 export default router;
