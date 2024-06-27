15ac5a5b0ae3283c45a0dbfea67fa36333e2d5a3
Yuta Sasaki <yuta.mybusiness@gmail.com>
2024-06-27 21:31:34 +0900

    add : postとdeleteを追加

--- router/users.ts
+++ router/users.ts
@@ -10,9 +10,20 @@ router.get("/", async (req: express.Request, res: express.Response) => {
 
 router.post("/", async (req: express.Request, res: express.Response) => {
   console.log("users POSTを受け付けました");
-  console.log("こんにちは");
-  const data = await knex("users").select();
-  res.json(data);
+  const reqData = req.body;
+  console.log("reqData: ", reqData);
+  const newUserId = await knex("users").insert(reqData).returning("id");
+
+  res.json(
+    `${reqData.name}さんの情報は id:${newUserId[0].id} でusersに登録しました`
+  );
+});
+
+router.delete("/", async (req: express.Request, res: express.Response) => {
+  console.log("users DELETEを受け付けました");
+  const userId = req.query.user_id;
+  await knex("users").where("id", userId).del();
+  res.json(`id:${userId}を削除しました`);
 });
 
 export default router;
